daa56f51860bbf95919696f1accd9b03bf1a565f
qualmaz <laskarnaved1@gmail.com>
2026-09-13 10:52:00 +0530

    Update 8th.cpp

--- 8th.cpp
+++ 8th.cpp
@@ -1,56 +1,49 @@
-// Define a class shape whose attributes are radius length and width. Calculate perimeter of the rectangle and circle. use constructors and destructors.
-#include<iostream>
-#include<math>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
+class Shape {
+private:
+    double radius, length, width;
 
-    class Shape{
-        private:
-        double radius,length,width;
+public:
+    // Single constructor handles all 3 attributes
+    Shape(double r, double l, double w) : radius(r), length(l), width(w) {
+        cout << "Shape created -> radius: " << radius
+             << ", length: " << length
+             << ", width: " << width << endl;
+    }
 
-        public:
-        Shape(double r): radius(r), length(0),width(0){
-            cout<< "Circle with perimeter with radius"<< radius << endl;
+    double circlePerimeter() {
+        return 2 * 3.14159 * radius;
+    }
 
+    double rectanglePerimeter() {
+        if (length > 0 && width > 0)
+            return 2 * (length + width);
+        else {
+            cout << "impossible\n";
+            return 0;
         }
-        Shape(double l,double w): length(l),width(w),radius(0){
-            cout<<" Rectangle with perimeter with length"<<length "width"<< width << endl;
+    }
 
-        }
-        ~Shape(){
-            cout<< "destroyed"<< endl;
+    ~Shape() {
+        cout << "destroyed" << endl;
+    }
+};
 
-        }
-         double rectanglePerimeter(){
-            if(length>0 && width>0)
-            {
-                return(2*(length+width));
-            
-            }
-            else{
-                 cout<<"impossible\n";
-            }
-         float circlePerimeter(){
-            return (2* 3.141325*radius);
+int main() {
+    double r, l, w;
 
-         }   
-             
-        
-               
-            
-         
-    };
-    int main();
-    float r;
-    double l,w;
-    cout<<"enter radius:\n";
-    cin>>r;
-    cout<<"enter length and width";
-    cin>>l>>r;
-    Shape s(r,l,w);
-    cout<<"\nPerimeter of circle ="<< s.circlePerimeter()<<endl;
-    cout<<"Perimeter of Rectangle ="<<s.rectanglePerimeter()<<endl;
+    cout << "Enter radius: ";
+    cin >> r;
+    cout << "Enter length and width: ";
+    cin >> l >> w;
+
+    Shape s(r, l, w);
+
+    cout << "\nPerimeter of circle = " << s.circlePerimeter() << endl;
+    cout << "Perimeter of rectangle = " << s.rectanglePerimeter() << endl;
 
     return 0;
 }
-
